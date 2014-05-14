7e90cc8703f94b2ce2f5892eedf2d69b6884ee7a
Tomasz Wiklo <tomaszwiklo@koszmail.pl>
2014-05-14 10:38:43 +0200

    pracuje nad zmiana funkcji dodawanie

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,4 @@
-def dodawanie():
-    a = int(input('Wprowadz pierwsza liczbe '))
-    b = int(input('Wprowadz druga liczbe '))
-    print(a+b)
+def dodawanie(a, b):
+    return a+b
     
-dodawanie()
+print(dodawanie())
